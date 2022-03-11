a496f030da2b0aa65bee25bafec9c31cf0538085
medram <mramouchy@gmail.com>
2022-03-11 18:25:47 +0100

    Added: media & static files path.

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('license_checker.urls'))
 ]
+
+urlpatterns.extend(
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+
+urlpatterns.extend(
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
