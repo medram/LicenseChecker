c3d1406a578bc96dc0c433f0c0a36b3286172577
medram <mramouchy@gmail.com>
2022-03-13 17:11:42 +0100

    Updated: mysql settings has been added

--- app/settings.py
+++ app/settings.py
@@ -97,6 +97,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.getenv('DB_NAME'),
+        # 'USER': os.getenv('DB_USER'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        # 'PORT': os.getenv('DB_PORT', '3306')
     }
 }
 
